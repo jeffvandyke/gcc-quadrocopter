1bbef678d9c1907baed9034a7082f98367f639c6
Cornelius Kloosterman <kloostermancw1@gcc.edu>
2015-03-25 15:37:22 -0400

    Revert "Made some changes to K filter test stub"
    This reverts commit 51ec685a6f2c5242d5c16b57277b67ead6026f12.

--- K filter work/main.cpp
+++ K filter work/main.cpp
@@ -5,10 +5,7 @@ using namespace std;
 
 int main() {
     KalmanFilter kf;
-    kf.initialize(-15, 2, 1);
-    for(int i = 0; i < 700; i++) {
-        kf.assignSensorValues(...);
-    	kf.predictAndUpdate();
-    }
+    kf.initialize();
+	kf.predictAndUpdate();
 	return 0;
 }
